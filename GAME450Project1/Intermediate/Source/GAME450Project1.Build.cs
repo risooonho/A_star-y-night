37c1938675ea1a5984989302509e7346ad419e66
SidneyHopson <50920818+SidneyHopson@users.noreply.github.com>
2020-03-12 18:42:23 -0400

    Alpha
    First Complete Version

---
+++ GAME450Project1/Intermediate/Source/GAME450Project1.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class GAME450Project1 : ModuleRules
+{
+	public GAME450Project1(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
---
+++ GAME450Project1/Intermediate/Source/GAME450Project1.cpp
@@ -0,0 +1,4 @@
+#include "CoreTypes.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultModuleImpl, GAME450Project1, "GAME450Project1");
